2946d7d1c011e48726c7b0a9149696c0fcf7d797
jonasm <jonasm@uni-koblenz.de>
2018-07-17 10:22:26 +0200

    Extended apply script by Install vim Plugins

--- apply.sh
+++ apply.sh
@@ -4,3 +4,8 @@ set -x #echo on
 cp .vimrc ~/
 cp .tmux.conf ~/
 cp own.sty ~/texmf/tex/latex/jonasOwn/
+
+# install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# install vim Plugins via Vundle
+vim +PluginInstall +qall
